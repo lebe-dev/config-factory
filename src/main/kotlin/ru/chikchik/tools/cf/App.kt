f33cf136d8f04df64ee9ffec3022d717920e042d
Evgeny Lebedev <duke.tougu@gmail.com>
2018-12-24 22:44:52 +0300

    [~] app class: show version; refactoring

--- src/main/kotlin/ru/chikchik/tools/cf/App.kt
+++ src/main/kotlin/ru/chikchik/tools/cf/App.kt
@@ -5,8 +5,10 @@ import org.apache.commons.cli.HelpFormatter
 import org.apache.commons.cli.Option
 import org.apache.commons.cli.Options
 import org.slf4j.LoggerFactory
-import ru.chikchik.tools.cf.service.ConfigManager
 import ru.chikchik.tools.cf.service.ConfigProducer
+import ru.chikchik.tools.cf.service.ConfigService
+import ru.chikchik.tools.cf.service.FileConfigProducer
+import ru.chikchik.tools.cf.service.FileConfigService
 import java.io.File
 
 
@@ -15,29 +17,13 @@ class App {
     companion object {
         private val log = LoggerFactory.getLogger(App::class.java)
 
+        private const val VERSION = "1.0.1"
+
         const val OUTPUT_DIRECTORY = "output"
 
-        private const val CONFIG_FILE = "config-factory.conf"
+        internal const val CONFIG_FILE = "config-factory.conf"
         private const val TEMPLATE_FILE_OPTION = "t"
         private const val OUTPUT_PATH_OPTION = "o"
-
-        private fun getCommandLineOptions(): Options =
-            Options().apply {
-                addOption(
-                    Option.builder(TEMPLATE_FILE_OPTION).argName("file-name").longOpt("template-file")
-                                                        .desc("template file")
-                                                        .optionalArg(false)
-                                                        .hasArg(true).required().build()
-                )
-                addOption(
-                    Option.builder(OUTPUT_PATH_OPTION).argName("path").desc("output path")
-                                                      .longOpt("output-path")
-                                                      .hasArg(true)
-                                                      .optionalArg(true)
-                                                      .required(false)
-                                                      .build()
-                )
-            }
 
         @JvmStatic
         fun main(args: Array<String>) {
@@ -45,23 +31,22 @@ class App {
             val helpFormatter = HelpFormatter()
 
             try {
+                log.info("=".repeat(32))
+                log.info("\n\tCONFIG FACTORY $VERSION\n")
+                log.info("  Mass config file generator \n")
+                log.info("-".repeat(32))
 
                 val cmd = DefaultParser().parse(commandOptions, args)
 
-                log.info("=".repeat(64))
-                log.info(" CONFIG FACTORY ")
-                log.info("=".repeat(64))
-
-                val configManager = ConfigManager()
-                val config = configManager.load(File(CONFIG_FILE))
+                val configService: ConfigService = FileConfigService()
+                val config = configService.load(File(CONFIG_FILE))
 
                 if (config.isPresent) {
                     log.debug("+ config has been found")
 
-                    val outputPath = if (cmd.hasOption(OUTPUT_PATH_OPTION)) {
-                        cmd.getOptionValue(TEMPLATE_FILE_OPTION) } else { OUTPUT_DIRECTORY }
+                    val outputPath = cmd.getOptionValue(TEMPLATE_FILE_OPTION) ?: OUTPUT_DIRECTORY
 
-                    val configProducer = ConfigProducer(cmd.getOptionValue(TEMPLATE_FILE_OPTION))
+                    val configProducer: ConfigProducer = FileConfigProducer(cmd.getOptionValue(TEMPLATE_FILE_OPTION))
 
                     val configFiles = configProducer.produce(
                         profiles = config.get().profiles,
@@ -71,7 +56,7 @@ class App {
                     )
 
                     if (configFiles.isPresent) {
-                        log.info("[+] created config files: ${configFiles.get().size} at path '$outputPath'")
+                        log.info("[+] created config files (${configFiles.get().size}) at path '$outputPath'")
                     }
 
                 } else {
@@ -83,6 +68,26 @@ class App {
                 log.debug(e.message, e)
             }
         }
+
+        private fun getCommandLineOptions(): Options =
+                Options().apply {
+                    addOption(
+                            Option.builder(TEMPLATE_FILE_OPTION).argName("file-name")
+                                                                .longOpt("template-file")
+                                                                .desc("template file")
+                                                                .optionalArg(false)
+                                                                .hasArg(true)
+                                                                .required().build()
+                    )
+                    addOption(
+                            Option.builder(OUTPUT_PATH_OPTION).argName("path").desc("output path")
+                                                              .longOpt("output-path")
+                                                              .hasArg(true)
+                                                              .optionalArg(true)
+                                                              .required(false)
+                                                              .build()
+                    )
+                }
 
     }
 
